af45e2130376b5494ecd50c5f7e27f9cbf271a04
carpatex <imls.ramirez.tomas@gmail.com>
2024-09-18 19:51:41 -0300

    gsettings

---
+++ arch/amd_a10_7860k/home/tomas/.local/bin/set_gsettings_opts.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 9'
+gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 9'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Noto Sans Mono 9'
