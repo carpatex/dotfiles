46280bf5d839eca30665782b4b16483ef8983750
carpatex <imls.ramirez.tomas@gmail.com>
2024-09-27 17:18:11 -0300

    nashei1

--- arch/amd_a10_7860k/home/tomas/.local/bin/set_st_def_keyboard.sh
+++ arch/amd_a10_7860k/home/tomas/.local/bin/set_st_def_keyboard.sh
@@ -3,3 +3,4 @@ ln -rsf $HOME/.config/foot/foot_def.ini $HOME/.config/foot/foot.ini
 cd $HOME/devel/projects/dotfiles/random/suckless/st_def_keyboard
 make 
 make PREFIX=$HOME/.local install
+make clean
--- arch/intel_i3_11th/home/tomas/.local/bin/set_st_laptop_keyboard.sh
+++ arch/intel_i3_11th/home/tomas/.local/bin/set_st_laptop_keyboard.sh
@@ -3,3 +3,4 @@ ln -rsf $HOME/.config/foot/foot_laptop.ini $HOME/.config/foot/foot.ini
 cd $HOME/devel/projects/dotfiles/random/suckless/st_laptop_keyboard
 make 
 make PREFIX=$HOME/.local install
+make clean
