362ed6477a22227e9a4b8d330b3229357a6c9c95
carpatex <imls.ramirez.tomas@gmail.com>
2025-02-08 18:07:49 -0300

    nashe

--- arch/intel_i3_11th/home/tomas/.local/bin/set_gsettings_opts.sh
+++ arch/intel_i3_11th/home/tomas/.local/bin/set_gsettings_opts.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark-compact'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 gsettings set org.gnome.desktop.interface font-name 'Noto Sans 12'
 gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 12'
