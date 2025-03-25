4bb58c75aba33e2a29f89c3a6d34d7114107ec1f
carpatex <imls.ramirez.tomas@gmail.com>
2025-03-25 19:51:50 -0300

    nashe

--- arch/amd_a10_7860k/home/tomas/.local/bin/status.sh
+++ arch/amd_a10_7860k/home/tomas/.local/bin/status.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-while true; do
-	TIME=$(date +"%R")
-	DATE=$(date +"%F")
-	echo "Battery: Date: $DATE | Time: $TIME"
-	sleep 5
-done
+TIME=$(date +"%R")
+DATE=$(date +"%F")
+echo "Date: $DATE | Time: $TIME"
---
+++ void/amd_a10_7860k/home/tomas/.local/bin/status_loop.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while true; do
+	status.sh	
+	sleep 5
+done
