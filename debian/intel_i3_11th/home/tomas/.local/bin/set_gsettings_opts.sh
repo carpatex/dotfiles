bcbfbf6024219dda20e18a5b21bf6397aea2712f
carpatex <imls.ramirez.tomas@gmail.com>
2025-06-10 16:45:42 -0300

    nashe

---
+++ debian/intel_i3_11th/home/tomas/.local/bin/set_gsettings_opts.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark-compact'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 11'
+gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Noto Sans Mono 11'
